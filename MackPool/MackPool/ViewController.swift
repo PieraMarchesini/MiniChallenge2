ac33dae074da260b970235516266cbfdf577aba6
Piera Marchesini <pieracarolina@yahoo.com.br>
2017-05-04 11:42:25 -0300

    Esqueleto do wireframe mais alteração no Search Bar

--- MackPool/MackPool/ViewController.swift
+++ MackPool/MackPool/ViewController.swift
@@ -17,13 +17,7 @@ class ViewController: UIViewController, PageViewControllerDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    }    
 
     // MARK: - Navigation
 
