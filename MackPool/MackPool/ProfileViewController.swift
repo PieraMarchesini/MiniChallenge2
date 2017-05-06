91b3e33b196bdf3c88408b2eef42411c143140aa
Piera Marchesini <pieracarolina@yahoo.com.br>
2017-05-06 16:29:53 -0300

    Mudança para tudo em português e Confirmação para log out

--- MackPool/MackPool/ProfileViewController.swift
+++ MackPool/MackPool/ProfileViewController.swift
@@ -11,8 +11,18 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     @IBAction func logOut(_ sender: Any) {
-        FirebaseController.instance.signUserOut()
-        self.performSegue(withIdentifier: "Logout", sender: self)
+        let lougOutAlert = UIAlertController(title:"Sair do Mack Pool?", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction(title: "Sair", style: UIAlertActionStyle.default, handler: { (UIAlertAction) in
+            //Log out do aplicativo
+            FirebaseController.instance.signUserOut()
+            self.performSegue(withIdentifier: "Logout", sender: self)
+        })
+        let cancelAction = UIAlertAction(title: "Cancelar", style: UIAlertActionStyle.destructive, handler: nil)
+        lougOutAlert.view.tintColor = UIColor(hex: "e11423")
+        lougOutAlert.addAction(cancelAction)
+        lougOutAlert.addAction(okAction)
+        lougOutAlert.preferredAction = cancelAction
+        self.present(lougOutAlert, animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
