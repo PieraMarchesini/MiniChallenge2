924f2fe8a309419769082fc11eec2b10641cffd3
Piera Marchesini <pieracarolina@yahoo.com.br>
2017-05-07 18:58:13 -0300

    Cadastro funcionando + comportamento estranho do DatePicker

--- MackPool/MackPool/RegisterViewController.swift
+++ MackPool/MackPool/RegisterViewController.swift
@@ -14,38 +14,36 @@ class RegisterViewController: UIViewController, UIPickerViewDelegate {
     
     @IBAction func dateOfBirthPressed(_ sender: UITextField) {
         //Create the view
-    
         let inputView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 240))
         
-        let datePickerView  : UIDatePicker = UIDatePicker(frame: CGRect(x: 0, y: 40, width: 0, height: 0))
+        let datePickerView: UIDatePicker = UIDatePicker(frame: CGRect(x: 0, y: 40, width: 0, height: 0))
         datePickerView.datePickerMode = UIDatePickerMode.date
         inputView.addSubview(datePickerView) // add date picker to UIView
         
         let doneButton = UIButton(frame: CGRect(x: (self.view.frame.size.width/2) - (100/2), y: 0, width: 100, height: 50))
         doneButton.setTitle("Done", for: UIControlState.normal)
-        doneButton.setTitle("Done", for: UIControlState.highlighted)
+        //doneButton.setTitle("Done", for: UIControlState.highlighted)
         doneButton.setTitleColor(UIColor.black, for: UIControlState.normal)
         doneButton.setTitleColor(UIColor.gray, for: UIControlState.highlighted)
         
         inputView.addSubview(doneButton) // add Button to UIView
         
-        doneButton.addTarget(self, action: Selector(("doneButton:")), for
-            : UIControlEvents.touchUpInside) // set button click event
+        doneButton.addTarget(self, action: #selector(doneButton(sender:)), for: .touchUpInside)
+        //doneButton.addTarget(self, action: Selector(("doneButton:")), for
+            //: UIControlEvents.touchUpInside) // set button click event
         
         sender.inputView = inputView
-        datePickerView.addTarget(self, action: Selector(("handleDatePicker:")), for: UIControlEvents.valueChanged)
-        
-        handleDatePicker(sender: datePickerView) // Set the date on start.
+        datePickerView.addTarget(self, action: #selector(handleDatePicker(sender:)), for: .valueChanged)
+        //handleDatePicker(sender: datePickerView) // Set the date on start.
     }
     
     func handleDatePicker(sender: UIDatePicker) {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.dateFormat = "dd/MM/yyyy"
         dateOfBirth.text = dateFormatter.string(from: sender.date)
     }
     
-    func doneButton(sender:UIButton)
-    {
+    func doneButton(sender:UIButton) {
         dateOfBirth.resignFirstResponder() // To resign the inputView on clicking done.
     }
     
