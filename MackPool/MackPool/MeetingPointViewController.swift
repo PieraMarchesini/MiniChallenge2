acced59b4fd8feb89a77ba20c6a07a627d2ced6f
Piera Marchesini <pieracarolina@yahoo.com.br>
2017-05-08 12:36:30 -0300

    Tela de criar grupos

---
+++ MackPool/MackPool/MeetingPointViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MeetingPointViewController.swift
+//  MackPool
+//
+//  Created by Piera Marchesini on 08/05/17.
+//  Copyright © 2017 Piera Marchesini. All rights reserved.
+//
+
+import UIKit
+
+class MeetingPointViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    
+    
+        // Deixar a search bar branca e o ícone.
+        let textFieldInsideSearchBar = searchBar.value(forKey: "searchField") as? UITextField
+        textFieldInsideSearchBar?.textColor = UIColor.white
+        
+        let textFieldInsideSearchBarLabel = textFieldInsideSearchBar!.value(forKey: "placeholderLabel") as? UILabel
+        textFieldInsideSearchBarLabel?.textColor = UIColor.white
+        
+        let clearButton = textFieldInsideSearchBar?.value(forKey: "clearButton") as! UIButton
+        clearButton.setImage(clearButton.imageView?.image?.withRenderingMode(.alwaysTemplate), for: .normal)
+        clearButton.tintColor = UIColor.white
+        
+        let glassIconView = textFieldInsideSearchBar?.leftView as? UIImageView
+        
+        glassIconView?.image = glassIconView?.image?.withRenderingMode(.alwaysTemplate)
+        glassIconView?.tintColor = UIColor.white
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
