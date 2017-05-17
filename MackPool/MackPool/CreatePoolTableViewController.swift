4fa4c51afd51b6be3ff805efeda51a7920500ad2
Brendoon <brendoon-ryos@live.com>
2017-05-17 19:21:19 -0300

    Arrumando o merge

--- MackPool/MackPool/CreatePoolTableViewController.swift
+++ MackPool/MackPool/CreatePoolTableViewController.swift
@@ -10,11 +10,7 @@ import UIKit
 import CoreLocation
 
 protocol MeetingPointViewControllerDelegate {
-<<<<<<< HEAD
     func didReceiveAddressFromMeetingPointViewController(address: String?, coordinate: CLLocationCoordinate2D?)
-=======
-    func didReceiveAddressFromMeetingPointViewController(address: String)
->>>>>>> 38656614f850fa1c54ed7f53d7266aa1e727cf62
 }
 
 class CreatePoolTableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource, MeetingPointViewControllerDelegate {
@@ -171,24 +167,6 @@ class CreatePoolTableViewController: UITableViewController, UIPickerViewDelegate
         }
     }
     
-<<<<<<< HEAD
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "meetingPoint" {
-            if let destination = segue.destination as? MeetingPointViewController {
-                destination.delegate = self
-            }
-        }
-    }
-    
-    func didReceiveAddressFromMeetingPointViewController(address: String?, coordinate: CLLocationCoordinate2D?) {
-        place = CLLocation(latitude: (coordinate?.latitude)!, longitude: (coordinate?.longitude)!)
-=======
-    func didReceiveAddressFromMeetingPointViewController(address: String) {
-        print("Endereço: \(address)")
->>>>>>> 38656614f850fa1c54ed7f53d7266aa1e727cf62
-        pontoEncontro.text = address
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -201,6 +179,11 @@ class CreatePoolTableViewController: UITableViewController, UIPickerViewDelegate
                     destination.delegate = self
             }
         }
+    }
+    
+    func didReceiveAddressFromMeetingPointViewController(address: String?, coordinate: CLLocationCoordinate2D?) {
+        pontoEncontro.text = address
+        place = CLLocation(latitude: (coordinate?.latitude ?? 0), longitude: (coordinate?.longitude ?? 0))
     }
     
     
